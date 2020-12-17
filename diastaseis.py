3ceb6cc1325ace405d50973cc7e328874766ce5d
master <papachristos.andreas@gmail.com>
2020-12-17 22:15:57 +0200

    unknow

--- diastaseis.py
+++ diastaseis.py
@@ -24,36 +24,37 @@ def fixPaper(width, height):
     return (list)
 
 
-
 def matchPage(width, height):
-
     width *= 2
     list = fixPaper(1000, 700)
     for i in list:
         temp = []
-        if width/i[0] < 1 or width/i[1] < 1:            
-            if width/i[0] > width/i[1]:
-                if width/i[0] > 1:
+        if width / i[0] < 1 or width / i[1] < 1:
+            if width / i[0] > width / i[1]:
+                if width / i[0] > 1:
                     temp.append([i[1], i[0]])
-                else: 
+                else:
                     temp.append([i[0], i[1]])
-               # i[1] = height/i[1]
-            elif width/i[0] < width/i[1]:
-                if width/i[1] > 1:
+            # i[1] = height/i[1]
+            elif width / i[0] < width / i[1]:
+                if width / i[1] > 1:
                     temp.append([i[0], i[1]])
-                else:                 
+                else:
                     temp.append([i[0], i[1]])
-               # i[1] = height/i[0]
+            # i[1] = height/i[0]
 
         print(temp)
-def find4up(diaireteos, dieretis):  
-    an = (12,9,8,6,4,3,2)
-    
+
+
+def find4up(diaireteos, dieretis):
+    an = (12, 9, 8, 6, 4, 3, 2)
+
     for i in an:
-        if (diaireteos//dieretis) >= i:
-         return i  
+        if (diaireteos // dieretis) >= i:
+            return i
     return 0
-   
+
+
 """ def dokimi():
     if ppw/pw > 0 and pph/ph > 0 :
             if ppw/pw > pph/ph:
@@ -87,41 +88,42 @@ def find4up(diaireteos, dieretis):
                     z += 1 
  """
 
-    
+
 def matchMontaz(**kwargs):
-    ph = kwargs.get('pageHeight') +6  
+    ph = kwargs.get('pageHeight') + 6
     pph = kwargs.get('paperHeight')
     temp = []
     if kwargs.get('monofyllo', True):
-        pw = kwargs.get('pagewidth') +6
+        pw = kwargs.get('pagewidth') + 6
         ppw = kwargs.get('paperWidth')
-        temp.append([ppw//pw, pph//ph])
-        temp.append([ppw//ph, pph//pw])
+        temp.append([ppw // pw, pph // ph])
+        temp.append([ppw // ph, pph // pw])
         print(temp)
-        return(temp)                           
-    else:            
-        pw = (kwargs.get('pagewidth') *2) +6 
-        ppw = kwargs.get('paperWidth') #/2                   
-        temp.append([ppw//pw, find4up(pph, ph)]) #pph//ph]
-        temp.append([find4up(ppw, ph), pph//pw]) #ppw//ph            
-        #print(temp)
-        return(temp)
-   
-def betterUse(papers, page, monofyllo):
+        return (temp)
+    else:
+        pw = (kwargs.get('pagewidth') * 2) + 6
+        ppw = kwargs.get('paperWidth')  # /2
+        temp.append([ppw // pw, find4up(pph, ph)])  # pph//ph]
+        temp.append([find4up(ppw, ph), pph // pw])  # ppw//ph
+        print(temp)
+        return (temp)
 
-    embadonp = page[0]*page[1]   
+
+def betterUse(papers, page, monofyllo):
+    embadonp = page[0] * page[1]
     for p in papers:
-        m = matchMontaz(pagewidth=page[0], pageHeight=page[1], paperWidth=p[0], paperHeight=p[1], monofyllo=True)
+        m = matchMontaz(pagewidth=page[0], pageHeight=page[1], paperWidth=p[0], paperHeight=p[1], monofyllo=False)
         embadon = (p[0] * p[1])
         print(p)
         for p in m:
-            if (monofyllo):            
-             y= (p[0] * p[1]) * embadonp 
+            if (monofyllo):
+                y = (p[0] * p[1]) * embadonp
             else:
-                y= (p[0] * p[1]) * embadonp *2 
-            print(format(y/embadon, '.2f'))
+                y = (p[0] * p[1]) * embadonp * 2
+            print(format(y / embadon, '.2f'))
+
 
 if __name__ == '__main__':
-    #print(find4up(880, 80))
-    #matchMontaz(pagewidth=210, pageHeight=280, paperWidth=860, paperHeight=610, monofyllo=False)
-    betterUse([[610,860],[640,880],[700,1000]], [320,490], True)
+    # print(find4up(880, 80))
+    # matchMontaz(pagewidth=210, pageHeight=280, paperWidth=860, paperHeight=610, monofyllo=False)
+    betterUse([[610, 860], [640, 880], [720, 1020]], [230, 230], False)
