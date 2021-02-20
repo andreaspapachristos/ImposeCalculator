264681d84522e7b868be288a6d7f1056526f9364
master <papachristos.andreas@gmail.com>
2021-02-20 20:03:09 +0200

    rename main and main2

---
+++ main_.py
@@ -0,0 +1,13 @@
+from main import Ui_MainWindow
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+if __name__ == "__main__":
+
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
