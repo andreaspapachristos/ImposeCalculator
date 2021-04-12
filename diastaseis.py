1f9160802a3649d1800b77adc720610c63d165b3
Andreas Papachristos <papachristos.andreas@gmail.com>
2021-04-12 08:09:10 +0300

    Update diastaseis.py
    remove brackets from licence

--- diastaseis.py
+++ diastaseis.py
@@ -1,6 +1,6 @@
 # from itertools import chain
 '''''
-Copyright [2021] [Andreas Papachristos]
+Copyright 2021 Andreas Papachristos
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
