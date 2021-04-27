0736fcaef71c2581636c24c55deae78b5f0360f8
master <papachristos.andreas@gmail.com>
2021-04-27 21:13:17 +0300

    change gui for work and turn

--- main.py
+++ main.py
@@ -267,9 +267,9 @@ class Ui_MainWindow(object):
         self.toolButton.setText(_translate("MainWindow", "add"))
         #self.toolButton_2.setText(_translate("MainWindow", "..."))
         self.toolButton_3.setText(_translate("MainWindow", "rm"))
-        self.groupBox_8.setTitle(_translate("MainWindow", "Units"))
-        self.checkBox_3.setText(_translate("MainWindow", "milimeters"))
-        self.checkBox_4.setText(_translate("MainWindow", "centimeters"))
+        self.groupBox_8.setTitle(_translate("MainWindow", "Single Page"))
+        self.checkBox_3.setText(_translate("MainWindow", "Work and Turn"))
+        self.checkBox_4.setText(_translate("MainWindow", "Work and Tumble"))
         self.textBrowser.setHtml(_translate("MainWindow",
                                             "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
                                             "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
@@ -281,7 +281,7 @@ class Ui_MainWindow(object):
                                               "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
                                               "p, li { white-space: pre-wrap; }\n"
                                               "</style></head><body style=\" font-family:\'Noto Sans\'; font-size:10pt; font-weight:400; font-style:normal;\">\n"
-                                              "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:8pt;\">Units has effect only when adding or editing papers</span></p></body></html>"))
+                                              "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:8pt;\">IF Single Page Impose</span></p></body></html>"))
         self.checkBox_5.setText(_translate("MainWindow", "Auto- Draw"))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), _translate("MainWindow", "preferences"))
         self.lineEdit.setValidator(QtGui.QIntValidator())
