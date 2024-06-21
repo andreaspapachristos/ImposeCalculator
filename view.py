4ea38aeda5e476e910882e10c125161f889a4cf1
Andreas <papachristos.andreas@gmail.com>
2024-06-21 12:46:05 +0300

    TXT information about paper size rotate

--- view.py
+++ view.py
@@ -80,7 +80,10 @@ class Ui_MainWindow1(object):
         pen.setColor(QtGui.QColor("#EB5160"))
         painter.setPen(pen)
         painter.drawText(int((MainWindow.geometry().width())/2), 10, str(MainWindow.geometry().width()))
-        painter.drawText(10, int( (MainWindow.geometry().height())/2),  str(MainWindow.geometry().height()))    
+        painter.rotate(90)
+        painter.drawText(int((MainWindow.geometry().height())/2), 0, str(MainWindow.geometry().height()))
+        painter.rotate(-90)
+       # painter.drawText(10, int( (MainWindow.geometry().height())/2),  str(MainWindow.geometry().height()))    
         #z =
         #
         if paper[2] == 0:
